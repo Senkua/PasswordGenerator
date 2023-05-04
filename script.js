8d13932f59cc209674237f7591b8cc0d6885b2de
Liam <liam64575@icloud.com>
2023-05-04 17:06:57 -0400

    Added Structure/ Variables for password generation

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 // Assignment code here
+var length = Number(prompt("Enter a password length between 8 and 128"))
+charType = prompt("Enter a character type: special, numeric, uppercase, lowercase."),
+password = generatePassword(); 
+document.getElementById("display").value= password;
+document.getElementById('copy-btn').addEventListener('click', copypassword);
+
+function generatePassword() {
+  var charsets = {
+    lowercase: 'abcdefghijklmnopqrstuvwxyz',
+    uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    numeric: '0123456789',
+    special: ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~' };
+
 
 
 // Get references to the #generate element
